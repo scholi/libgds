2cfffd3c5c479d134f46dd11590f222fae7cb8aa
Olivier Scholder <o.scholder@gmail.com>
2013-09-05 14:54:11 +0200

    Add custom font

--- gds.py
+++ gds.py
@@ -1,19 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # *-* coding: utf-8 *-*
 
+# SVN FILE: $Id: gds.py 268 2012-09-04 07:55:49Z scholi $
+#
 # Project Name : GDSII library
 #
-# Author: scholi
+# Author: $Author: scholi $
 # Copyright: $Copyright$
-# Version: 1.0
-# Last Revision: $Date: 2011-08-17 10:27:55 +0200 (mer 17 aoû 2011) $
+# Version: $Revision: 268 $
+# Last Revision: $Date: 2012-09-04 09:55:49 +0200 (mar 04 sep 2012) $
 # Modified by: $LastChangedBy: scholi $
-# Last Modified: $LastChangedDate: 2011-08-17 10:27:55 +0200 (mer 17 aoû 2011) $
+# Last Modified: $LastChangedDate: 2012-09-04 09:55:49 +0200 (mar 04 sep 2012) $
 
 import struct
 import sys
 import binascii
 import math
+import os
 
 def MatrVectMul(A,v):
 	if A==[[1,0],[0,1]]:
@@ -112,8 +115,11 @@ class GDSII:
 			"\x2f\x03":"PLEX",
 			"\x36\x02":"FORMAT",
 			"\x37\x06":"MASK",
-			"\x38\x00":"ENDMASKS"}
+			"\x38\x00":"ENDMASKS",
+			"\x58\x00":"FBMS"}
+		self.IType=dict((k,v) for v,k in self.Type.items())
 		self.structs=[]
+		self.minimax=[[0,0],[0,0]]
 		self.objs=[]
 		self.M=[[1,0],[0,1]]
 		self.shift=[0,0]
@@ -164,8 +170,52 @@ class GDSII:
 		# pos is the position of the LL corner of the marker
 		# The marker is a cross (+) fitting in a (size x size) square.
 		# The arms have a width of width
-		self.addRect((pos[0]+(size-width)/2,pos[1],width,size),dose=dose,loop=loop)
-		self.addRect((pos[0],pos[1]+(size-width)/2,size,width),dose=dose,loop=loop)
+		a=(width-size)/2
+		b=(width-size)/2
+		self.addPoly([
+			pos[0]+a,pos[1],
+			pos[0]+b,pos[1],
+			pos[0]+b,pos[1]+a,
+			pos[0]+size,pos[1]+a,
+			pos[0]+size,pos[1]+b,
+			pos[0]+b,pos[1]+b,
+			pos[0]+b,pos[1]+size,
+			pos[0]+a,pos[1]+size,
+			pos[0]+a,pos[1]+b,
+			pos[0],pos[1]+b,
+			pos[0],pos[1]+a,
+			pos[0]+a,pos[1]+a],dose=dose,loop=loop)
+	def addSRef(self,struct, pos=(0,0), mag=1, angle=0):
+		self.addObj('SREF')
+		self.addObj('SNAME',struct)
+		if mag!=1:
+			self.addObj('MAG',float(mag))
+		if angle!=0:
+			self.addObj('ANGLE',float(angle))
+			A=[[math.cos(angle),-math.sin(angle)],[math.sin(angle),math.cos(angle)]]
+		self.addObj('XY',list(pos))
+		self.addObj('\x11\x00')
+	def addFBMS(self, pos, dose=1, width=0, layer=0, curvature=[], head=(0,0,0,0)):
+		self.addObj('FBMS')
+		self.addObj('LAYER',layer)
+		self.addObj('DATATYPE',self.doseEnc(dose))
+		if width>0:
+			self.addObj('WIDTH',width)
+		p=list(head)
+		if curvature==[]:
+			curvature=[0]*(1+len(pos)/2)
+		for i in range(len(pos)/2):
+			if i==0:
+				p+=[0]
+			elif curvature[i]>0:
+				p+=[2]
+			else:
+				p+=[1]
+			p+=pos[2*i:(2*i+2)]
+			p+=[curvature[i]]
+		self.addObj('XY',p)	
+		self.addObj('\x11\x00')
+			
 	def addARef(self, struct, pos=(0,0), mag=1, angle=0, array=(1,1),spacing=(5000,5000)):
 		self.addObj('AREF')
 		self.addObj('SNAME',struct)
@@ -193,10 +243,15 @@ class GDSII:
 		self.M=MatrMatrMul([[x,0],[0,y]],self.M)
 	def uvRotate(self, alpha=0):
 		alpha*=math.pi/180
-		self.M=MatrMatrMul([[math.cos(alpha),math.sin(alpha)],[-math.sin(alpha),cos(alpha)]],self.M)
+		self.M=MatrMatrMul([[math.cos(alpha),math.sin(alpha)],[-math.sin(alpha),math.cos(alpha)]],self.M)
+	def uvMirror(self, x=False, y=False):
+		self.M=MatrMatrMul([[ [1,-1][x],0],[0, [1,-1][y]]],self.M)
 	def getType(self, t):
 		if t in self.Type.values():
-			return self.Type.keys()[self.Type.values().index(t)]
+			for x in self.Type:
+				if t==self.Type[x]:
+					#print("%s"%(binascii.hexlify(x.encode('utf-8'))))
+					return x
 		else: return t
 
 	def uv2xy(self,v):
@@ -240,10 +295,17 @@ class GDSII:
 
 	def open(self, path):
 		self.objs=[]
+		bs=os.path.getsize(path)
 		f=open(path,"rb")
 		bLENGTH=f.read(2)
 		cs=[0,0]
+		lp=0
+		lo=None
 		while bLENGTH!='':
+			v=f.tell()*100/bs
+			if v>lp:
+				print("%i%% done"%(v))
+				lp=v
 			LENGTH=struct.unpack(">H",bLENGTH)[0]
 			TYPE=f.read(2)
 			PARAMS=f.read(LENGTH-4)
@@ -266,16 +328,42 @@ class GDSII:
 				elif self.Type[TYPE] == 'ENDSTR':
 					cs[1]=len(self.objs)
 					self.structs.append(cs)
-			if self.f==None:
-				ob={'TYPE':TYPE,'PARAMS':TXT}
-				self.objs.append(ob)
-			else:
-				if TYPE in self.Type: TYPE=self.Type[TYPE]
-				else: TYPE="??? (%s)"%(binascii.hexlify(TYPE))
-				if TYPE in ['BOUNDARY','PATH','SREF','AREF','TEXT','NODE','BOX']:
-					self.f.write("[%s]\n"%(TYPE))
-				else:
-					self.f.write("\t[%s]\t%s\n"%(TYPE,TXT))
+			if TYPE in self.Type: TYPE=self.Type[TYPE]
+			else: TYPE="??? (%s)"%(binascii.hexlify(TYPE))
+#			if self.f==None:
+#				ob={'TYPE':TYPE,'PARAMS':TXT}
+#				self.objs.append(ob)
+#			else:
+#				if TYPE in ['BOUNDARY','PATH','SREF','AREF','TEXT','NODE','BOX']:
+#					self.f.write("[%s]\n"%(TYPE))
+#				else:
+#					self.f.write("\t[%s]\t%s\n"%(TYPE,TXT))
+#				if TYPE in self.Type: TYPE=self.Type[TYPE]
+#				else: TYPE="??? (%s)"%(binascii.hexlify(TYPE))
+			if TYPE=='STRNAME':
+				t=TXT[0]
+				if t[-1]=='\x00': t=t[:-1]
+				ff=open(path+"_"+t+".svg","w")
+				ff.write("<svg xmlns=\"http://www.w3.org/2000/svg\">\n")
+				ff.write("<g transform=\"scale(1,-1)\">\n")
+			elif TYPE=='ENDSTR':
+				ff.write("</g>\n</svg>")
+				ff.close()
+			if TYPE in ['BOUNDARY','PATH','SREF','AREF','TEXT','NODE','BOX']:
+				lo=TYPE
+			elif TYPE=='SNAME':
+				los=TXT[0]
+				if los[-1]=='\x00': los=los[:-1]
+			elif TYPE=='XY':
+				if lo in ['BOUNDARY','PATH']:
+					ff.write("<path d=\"M %i,%i L"%(TXT[0],TXT[1]))
+					for x in zip(TXT[2::2],TXT[3::2]):
+						ff.write(" %i,%i"%x)
+					if lo=='PATH':
+						ff.write("\" fill=\"none\" stroke=\"black")
+					ff.write("\" />\n")
+				if lo in ['SREF','AREF']:
+					ff.write("<g transform=\"scale(1,-1)\"><text x=\"%i\" y=\"%i\" font-size=\"10000\">%s</text></g>\n"%(TXT[0],TXT[1],los))
 	def show(self,a=0,b=-1):
 		if b==-1: b=len(self.objs)
 		for x in self.objs[a:b]:
@@ -283,9 +371,9 @@ class GDSII:
 			if T in self.Type: TT=self.Type[T]
 			else: TT="??? (%s)"%(binascii.hexlify(T))
 			if TT in ['BOUNDARY','PATH','SREF','AREF','TEXT','NODE','BOX']:
-				print "[", TT, "]"
+				print("[", TT, "]")
 			else:
-				print "\t[",TT,"]", x['PARAMS']
+				print("\t[",TT,"]", x['PARAMS'])
 	def getLobjs(self,a=0,b=-1):
 		r=[]
 		co={}
@@ -330,10 +418,10 @@ class GDSII:
 		self.addObj('DATATYPE',self.doseEnc(dose))
 		self.addObj('WIDTH',width)
 		self.addObj('XY',pts)
-		# No idea what it is, but Raith write them for each line!
-		# It doesn't work if this command is not set!
 		if loop>1:
 			self.writeLoop(loop)
+		# No idea what it is, but Raith write them for each line!
+		# It doesn't work if this command is not set!
 		self.addObj('\x11\x00')
 	def playMacro(self):
 		for x in self.macro:
@@ -432,34 +520,116 @@ class GDSII:
 			self.addPoly([pos[0],pos[1],pos[0]+pos[2],pos[1],pos[0]+pos[2],pos[1]+pos[3],pos[0],pos[1]+pos[3],pos[0],pos[1]],dose=does,layer=layer,loop=loop)
 		else:
 			self.addPoly([pos[0],pos[1],pos[0],pos[1]+pos[3],pos[0]+pos[2],pos[1]+pos[3],pos[0]+pos[2],pos[1],pos[0],pos[1]],dose=dose,layer=layer,loop=loop)
-	def addText(self, pos, txt, height=None, mag=22.22222, layer=0, width=0, dose=1,angle=0,loop=None):
+	def addText(self, pos, txt, height=None, mag=22.22222, layer=0, width=0, dose=1,angle=0,loop=None, align='NW',custom=False, mirror=False):
+		# Custom Font
+		font={	'0':[[(0,0),(1,0),(1,2),(0,2),(0,0)]],
+			'1':[[(.5,0),(.5,2)]],
+			'2':[[(0,2),(1,2),(1,1),(0,1),(0,0),(1,0)]],
+			'3':[[(0,0),(1,0),(1,2),(0,2)],[(0,1),(1,1)]],
+			'4':[[(0,2),(0,1),(1,1)],[(1,2),(1,0)]],
+			'5':[[(0,0),(1,0),(1,1),(0,1),(0,2),(1,2)]],
+			'6':[[(1,2),(0,2),(0,0),(1,0),(1,1),(0,1)]],
+			'7':[[(0,2),(1,2),(0,0)],[(0,1),(1,1)]],
+			'8':[[(0,1),(1,1),(1,2),(0,2),(0,0),(1,0),(1,1)]],
+			'9':[[(0,0),(1,0),(1,2),(0,2),(0,1),(1,1)]],
+			'A':[[(0,0),(0,2),(1,2),(1,0)],[(0,1),(1,1)]],
+			'B':[[(0,0),(0,2),(1,1.5),(0,1),(1,0.5),(0,0)]],
+			'C':[[(1,0),(0,0),(0,2),(1,2)]],
+			'D':[[(0,0),(0,2),(1,1),(0,0)]],
+			'E':[[(1,0),(0,0),(0,2),(1,2)],[(0,1),(1,1)]],
+			'F':[[(0,0),(0,2),(1,2)],[(0,1),(1,1)]],
+			'G':[[(1,2),(0,2),(0,0),(1,0),(1,1),(0.5,1)]],
+			'H':[[(0,0),(0,2)],[(1,0),(1,2)],[(0,1),(1,1)]],
+			'I':[[(0.5,0),(0.5,2)],[(0,0),(1,0)],[(0,2),(1,2)]],
+			'J':[[(0,2),(1,2),(1,0),(0,0),(.5,.5)]],
+			'K':[[(0,0),(0,2)],[(1,2),(0,1),(1,0)]],
+			'L':[[(0,2),(0,0),(1,0)]],
+			'M':[[(0,0),(0,2),(0.5,1),(1,2),(1,0)]],
+			'N':[[(0,0),(0,2),(1,0),(1,2)]],
+			'O':[[(0,0),(1,0),(1,2),(0,2),(0,0)]],
+			'P':[[(0,0),(0,2),(1,2),(1,1),(0,1)]],
+			'Q':[[(0.5,0.5),(1,0),(1,2),(0,2),(0,0),(1,0)]],
+			'R':[[(0,0),(0,2),(1,2),(1,1),(0,1),(1,0)]],
+			'S':[[(0,0),(1,0),(1,1),(0,1),(0,2),(1,2)]],
+			'T':[[(0,2),(1,2)],[(0.5,0),(0.5,2)]],
+			'U':[[(0,2),(0,0),(1,0),(1,2)]],
+			'V':[[(0,2),(.5,0),(1,2)]],
+			'W':[[(0,2),(0,0),(.5,1),(1,0),(1,2)]],
+			'X':[[(0,0),(1,2)],[(0,2),(1,0)]],
+			'Y':[[(0,2),(.5,1),(1,2)],[(.5,1),(.5,0)]],
+			'Z':[[(0,2),(1,2),(0,0),(1,0)]],
+			'+':[[(0.5,0),(0.5,2)],[(0,1),(1,1)]],
+			'-':[[(0,1),(1,1)]],
+			'=':[[(0,.75),(1,.75)],[(0,.25),(1,.25)]],
+			'.':[[(.33,0),(.33,.33),(.66,.33),(.66,0),(.33,0)]],
+			'?':[[(0,0),(1,2),(0,2),(0,0),(1,0),(1,2)],[(0,2),(1,0)]],
+			' ':[]
+			}
 		# height is given in micron!
 		if loop==None:
 			loop=self.loops
 		if height!=None:
 			mag=height*22.22222
-		self.addObj('TEXT')
-		self.addObj('LAYER',layer)
-		self.addObj('TEXTTYPE',0)
-		self.addObj('DATATYPE',self.doseEnc(dose))
-		self.addObj('\x17\x02')
-		self.addObj('WIDTH',width)
-		self.addObj('STRANS',0)
-		if angle!=0:
-			self.addObj('ANGLE',angle*1.0)
-		self.addObj('MAG',mag)
-		self.addObj('XY',pos)
-		self.addObj('ASCII STRING',txt+'\x00')
-		if loop>1:
-			self.writeLoop(loop)
-		self.addObj('\x11\x00')
+		if not custom:
+			self.addObj('TEXT')
+			self.addObj('LAYER',layer)
+			self.addObj('TEXTTYPE',0)
+			self.addObj('DATATYPE',self.doseEnc(dose))
+			al={'NW':0,'N':1,'NE':2,'W':4,'C':5,'E':6,'SW':8,'S':9,'SE':10}
+			if type(align)==type(1):
+				self.addObj('\x17\x02',align)
+			else:
+				self.addObj('\x17\x02',al[align])
+			self.addObj('WIDTH',width)
+			self.addObj('STRANS',0)
+			if angle!=0:
+				self.addObj('ANGLE',angle*1.0)
+			self.addObj('MAG',mag)
+			self.addObj('XY',pos)
+			self.addObj('ASCII STRING',txt+'\x00')
+			if loop>1:
+				self.writeLoop(loop)
+			self.addObj('\x11\x00')
+		else:
+			bckShift=self.shift
+			bckMatr=self.M
+			length=2*len(txt)-1
+			self.uvShift(x=pos[0],y=pos[1])
+			dx=length/2.0
+			dy=height/2.0
+			if 'W' in align:
+				dx=0
+			elif 'E' in align:
+				dx=-length
+			if 'N' in align:
+				dy=0
+			elif 'S':
+				dy=-height
+			if angle!=0:
+				self.uvRotate(angle)
+			if mirror:
+				self.uvMirror(x=True)
+				dx-=length
+			self.uvScale(x=500*height,y=500*height)
+			for x in enumerate(txt):
+				t='?'
+				if x[1] in font:
+					t=x[1]
+				else:
+					if x[1].upper() in font:
+						t=x[1].upper()
+				for k in font[t]:
+					self.addLine(reduce(lambda a,b: list(a)+list(b), map(lambda z: (dx+z[0]+x[0]*2,dy+z[1]),k)),dose=dose,layer=layer,loop=loop,width=width)
+			self.M=bckMatr
+			self.shift=bckShift
+			
 if __name__ == "__main__":
 	## Exemple. Do a grating
 	# create the GDSII object
 	g=GDSII()
 	if len(sys.argv)>1:
 		g.open(sys.argv[1])
-		g.show()
+#		g.show()
 	else:
 		# add automatically the standard headers
 		g.new('TEST')
